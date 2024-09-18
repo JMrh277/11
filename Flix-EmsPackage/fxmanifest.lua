f41c06735537d262642328468db8fb342187fabd
JMrh277 <riteishdr@gmail.com>
2024-09-18 05:38:56 -0700

    Add files via upload

---
+++ Flix-EmsPackage/fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'adamant'
+games { 'gta5' }
+--[[
+
+					███████╗██╗░░░░░██╗██╗░░██╗
+					██╔════╝██║░░░░░██║╚██╗██╔╝
+					█████╗░░██║░░░░░██║░╚███╔╝░
+					██╔══╝░░██║░░░░░██║░██╔██╗░
+					██║░░░░░███████╗██║██╔╝╚██╗
+					╚═╝░░░░░╚══════╝╚═╝╚═╝░░╚═╝ 
+
+    Made By : MoSattout#2632
+    ♥ شكرا لك على الثقة ♥
+    https://discord.gg/5vE6Drh
+    يرجى عدم تعديل اي شيء هنا
+]]
+dependency "vrp"
+client_scripts {"ClientSide/lib/*.lua",'ClientSide/client.lua'}
+server_scripts {'@vrp/lib/utils.lua','ServerSide/server.lua'}
+files {'Front-End/index.html','Front-End/style.css','Front-End/main.js','Front-End/background.png','Front-End/assets/*.png'}
+ui_page {'Front-End/index.html'}
+--[[
+	███████╗██╗░░░░░██╗██╗░░██╗
+	██╔════╝██║░░░░░██║╚██╗██╔╝
+	█████╗░░██║░░░░░██║░╚███╔╝░
+	██╔══╝░░██║░░░░░██║░██╔██╗░
+	██║░░░░░███████╗██║██╔╝╚██╗
+	╚═╝░░░░░╚══════╝╚═╝╚═╝░░╚═╝ 
+    Made By : MoSattout#2632
+    https://discord.gg/5vE6Drh
+    يرجى عدم تعديل اي شيء هنا
+]]
+
+
+
+ client_script "RAM.lua"
